24730618585f5dddab08f5e2e31c567ac071873f
roy astronomia <roy.astronomia@gmail.com>
2021-11-29 18:35:51 +1300

    Add entities and graphql packages

---
+++ GraphQL.Sandbox.WebApi/Entities/Grade.cs
@@ -0,0 +1,13 @@
+﻿namespace GraphQL.Sandbox.WebApi.Entities
+{
+    public class Grade
+    {
+        public int GradeId { get; set; }
+
+        public string GradeName { get; set; }
+
+        public string Section { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+}
