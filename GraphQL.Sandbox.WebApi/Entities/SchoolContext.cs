76e68c9f055e100d65e732b60657452f239775c3
roy astronomia <roy.astronomia@gmail.com>
2021-11-29 19:18:11 +1300

    Add EF migrations

---
+++ GraphQL.Sandbox.WebApi/Entities/SchoolContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GraphQL.Sandbox.WebApi.Entities
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(): base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Grade> Grades { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;Database=SchoolDB;Trusted_Connection=True;");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
